6bdc68c03dba467da6aee8f5ee0a39249053f818
Arnaud <hayon.arnaud@gmail.com>
2025-07-15 11:26:50 +0200

    initialisation

---
+++ Oniri/Oniri/OniriApp.swift
@@ -0,0 +1,17 @@
+//
+//  OniriApp.swift
+//  Oniri
+//
+//  Created by Arnaud Hayon on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct OniriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Oniri/OniriTests/OniriTests.swift
@@ -0,0 +1,17 @@
+//
+//  OniriTests.swift
+//  OniriTests
+//
+//  Created by Arnaud Hayon on 15/07/2025.
+//
+
+import Testing
+@testable import Oniri
+
+struct OniriTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
