e6ef52776a9fbc8b5be0042d0e92d4d3640fc675
apprenant152 <apprenant152@10-80-59-24.0200.sim.fr.iosda.org>
2025-07-17 16:36:57 +0200

    Created Model & ViewModel of collection

---
+++ Oniri/Oniri/Features/CardCollection/CollectionModel.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionModel.swift
+//  Oniri
+//
+//  Created by apprenant152 on 17/07/2025.
+//
+
+import Foundation
+
+struct Collection{
+    var nbCard: Int
+    var cards: [Cards]
+}
+
---
+++ Oniri/Oniri/Features/CardCollection/CollectionViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewModel.swift
+//  Oniri
+//
+//  Created by apprenant152 on 17/07/2025.
+//
+
+import Foundation
+
+class CollectionViewModel{
+    
+}
