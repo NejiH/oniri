01b8366fe5f1e819455c4ffa666110212d6c6a47
Sébastien <sebbody79400@gmail.com>
2025-07-18 14:13:20 +0200

    feature: SuccesView & add Font custom

---
+++ Oniri/Oniri/Features/Cards/Success/SuccesView.swift
@@ -0,0 +1,60 @@
+//
+//  SuccesView.swift
+//  Oniri
+//
+//  Created by Sebastien Besse on 18/07/2025.
+//
+
+import SwiftUI
+
+struct SuccesView: View {
+    var body: some View {
+        ZStack{
+            VStack(spacing: -1){
+                Rectangle()
+                    .fill(.sofGreen)
+                Rectangle()
+                    .fill(.softPink)
+            }
+            .ignoresSafeArea()
+            VStack(spacing: 10){
+                Rectangle()
+                    .fill(.softPink)
+                    .frame(width: 200, height: 2)
+                    
+                Text("Félicitations")
+                    .font(.custom("DelaGothicOne-Regular", size: 46))
+                    .foregroundStyle(.primaryBeige)
+                    .bold()
+                Text("Tu as complété l’exploration et obtenu l'artefact du Rêve#1 !")
+                    .foregroundStyle(.primaryBeige)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 30)
+                Image("card-patience")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: .black.opacity(0.6), radius: 12, x: 10, y: 20)
+                
+                Button {
+                    print("retour")
+                } label: {
+                    Text("Retour")
+                        .foregroundStyle(.primaryBeige)
+                        .padding()
+                        .background{
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(.sofGreen)
+                                .frame(width: 150)
+                        }
+                }
+                .padding(.vertical, 10)
+            }
+            .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    SuccesView()
+}
