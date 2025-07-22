1576d352545183df0771fb9d5dc90ef4d8688abf
Sébastien <sebbody79400@gmail.com>
2025-07-22 14:21:52 +0200

    Feature: update rotation success card

--- Oniri/Oniri/Shared/Components/cardsRotateButton.swift
+++ Oniri/Oniri/Shared/Components/cardsRotateButton.swift
@@ -10,18 +10,32 @@ import SwiftUI
 struct CardsRotateButton: View {
     @State var isShowVerso: Bool = false
     @State var rotation: Double = 0
+    @State var rotationText: Double = 0
     var body: some View {
         VStack{
             Button {
-                withAnimation {
+                withAnimation(Animation.easeOut(duration: 1.5)) {
                     isShowVerso.toggle()
                 }
                 withAnimation(Animation.easeOut(duration: 1.5)) {
-                    rotation = 360
+                    rotation = -180
+                    rotationText = 180
                 }
             } label: {
                 if isShowVerso{
-                    CardDetail()
+                    ZStack{
+                        Image("verso-card")
+                            .resizable()
+                            .scaledToFit()
+                            .shadow(color: .black.opacity(0.6), radius: 12, x: 10, y: 20)
+                        VStack{
+                            Text("Il était une fois une artéfiacte qui était si puissant que cela rendez toutes les personnes qui posait leur regard sur la carte qui devint d'une extrême patience")
+                                .multilineTextAlignment(.center)
+                                .rotation3DEffect(.degrees(isShowVerso ? 180 : 0), axis: (x: 0, y: 1, z: 0))
+                        }
+                        .padding(.horizontal, 80)
+                       
+                    }
                 }else{
                     Image("card-patience")
                         .resizable()
