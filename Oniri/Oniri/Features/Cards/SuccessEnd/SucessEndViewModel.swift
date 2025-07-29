73f76f927b20918a4db4184868df1a02c03584e5
Arnaud <hayon.arnaud@gmail.com>
2025-07-29 14:07:05 +0200

    add: success end view et viewmodel

---
+++ Oniri/Oniri/Features/Cards/SuccessEnd/SucessEndViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  SucessEndViewModel.swift
+//  Oniri
+//
+//  Created by Arnaud Hayon on 29/07/2025.
+//
+
+import Foundation
+import SwiftUI
+
+@Observable
+class SuccessEndViewModel {
+    var isBack: Bool = false
+
+    func goBack() {
+        isBack = true
+    }
+    
+    var successLogo: Image {
+        return Image("icon-faces-light")
+    }
+    
+    var successCards: Image {
+        return Image("cards-success")
+    }
+    
+    var successText: String {
+        let text = "Vous avez avancé avec patience et fait preuve d’empathie, de douceur et d’écoute tout au long de votre aventure. Vous ne cherchez pas à imposer votre volonté, mais à comprendre l’autre, à ressentir avant d’agir.\n\nVous incarnez une présence stable, réconfortante, presque maternelle. "
+        
+        return text
+    }
+    
+}
+
+
