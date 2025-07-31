755ef1f3ffff73129ebadbbb9da23cb143d8175c
Arnaud <hayon.arnaud@gmail.com>
2025-07-31 11:08:52 +0200

    update: SuccessViewModel

--- Oniri/Oniri/Features/Cards/SuccessEnd/SucessEndViewModel.swift
+++ Oniri/Oniri/Features/Cards/SuccessEnd/SucessEndViewModel.swift
@@ -25,7 +25,7 @@ class SuccessEndViewModel {
     }
     
     var successText: String {
-        let text = "Vous avez avancé avec patience et fait preuve d’empathie, de douceur et d’écoute tout au long de votre aventure. Vous ne cherchez pas à imposer votre volonté, mais à comprendre l’autre, à ressentir avant d’agir.\n\nVous incarnez une présence stable, réconfortante, presque maternelle. "
+        let text = "Vous avez avancé avec patience et fait preuve d’empathie, de douceur et d’écoute tout au long de votre aventure. Vous ne cherchez pas à imposer votre volonté, mais à comprendre l’autre, à ressentir avant d’agir.\n\nVous incarnez une présence stable et réconfortante. "
         
         return text
     }
